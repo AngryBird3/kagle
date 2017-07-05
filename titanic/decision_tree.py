8783f82a5d04058299c5cb046afb9185b70ccd16
AngryBird3 <dhaaraa.darji@gmail.com>
2017-07-04 21:27:31 -0400

    Preprocess starting

--- titanic/decision_tree.py
+++ titanic/decision_tree.py
@@ -1,7 +1,10 @@
 """
 Jun 28th 2017
 Dhara
+
+Resource: https://blog.socialcops.com/engineering/machine-learning-python/
 """
+
 import pandas as pd
 
 '''
@@ -16,16 +19,42 @@ def read_data_frame(filepath):
 @param dataframe
 '''
 def analysis(df):
+    print("\n--------------------------------------------------------")
     print("What all columns/features we have?")
-    print(df[0])
+    print("---------------------------------------------------------")
+    print(df.columns)
+    print("\n---------------------------------------------------------")
     print("# of people who survived")
-    print(df['survived'].mean())
+    print("---------------------------------------------------------")
+    print(df['Survived'].mean())
+    print("\n---------------------------------------------------------")
     print("Let's see, how does data look like for each class?")
-    print(df.groupby('pclass').mean())
+    print("---------------------------------------------------------")
+    print(df.groupby('Pclass').mean())
 
 
     print("Ok, missing data?")
     print(df.count())
+
+def preprocess_df(df):
+    '''
+    Data looks likes this for each column
+    PassengerId    891 - ID
+    Survived       891 - 0 = No, 1 = Yes
+    Pclass         891 - Passenger class(1 = first, 2 = second, 3 = third)
+    Name           891 - Name
+    Sex            891 - Sex
+    Age            714 - Age
+    SibSp          891 - # of siblings/spouces aboard
+    Parch          891 - # of parents/children aboard
+    Ticket         891 - Ticket #
+    Fare           891 - Passenger fare
+    Cabin          204 - Cabin
+    Embarked       889 = Port of embarkation(C = Cherbourg, Q = Queentown, S = Southampton)
+    dtype: int64
+    '''
+    processed_df = df.copy()
+    processed_df.drop(['Name', 'Ticket', 'cabin'])
 
 def main(path='data/train.csv'):
     df = read_data_frame(path)
