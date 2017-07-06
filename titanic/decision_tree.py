c9c95a218a759e2596de72ad04fc11ea5b0d85d1
Darji <dhaaraab@amazon.com>
2017-07-05 20:30:49 -0400

    Complete working decision tree #1

--- titanic/decision_tree.py
+++ titanic/decision_tree.py
@@ -6,6 +6,8 @@ Resource: https://blog.socialcops.com/engineering/machine-learning-python/
 """
 
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
 
 '''
 @param filepath
@@ -17,6 +19,7 @@ def read_data_frame(filepath):
 
 '''
 @param dataframe
+Does bunch of analysis
 '''
 def analysis(df):
     print("\n--------------------------------------------------------")
@@ -33,9 +36,14 @@ def analysis(df):
     print(df.groupby('Pclass').mean())
 
 
-    print("Ok, missing data?")
+    print("Ok, missing data?\n")
     print(df.count())
 
+'''
+Preprocess data
+@param df dataframe
+@returns dataframe
+'''
 def preprocess_df(df):
     '''
     Data looks likes this for each column
@@ -53,11 +61,55 @@ def preprocess_df(df):
     Embarked       889 = Port of embarkation(C = Cherbourg, Q = Queentown, S = Southampton)
     dtype: int64
     '''
+    #Dropping unwanted/not-needed columns
     processed_df = df.copy()
-    processed_df.drop(['Name', 'Ticket', 'cabin'])
+    processed_df = processed_df.drop(['Name', 'Ticket', 'Cabin', 'PassengerId', 'Fare'], axis=1)
+    #print("After dropping name, ticket, cabin, passengerId...")
+    #print(processed_df.count())
 
-def main(path='data/train.csv'):
-    df = read_data_frame(path)
+    #Sex has male/female lable, I want to make it 1=male and 0=female
+    le = LabelEncoder()
+    processed_df['Sex'] = le.fit_transform(processed_df['Sex'])
+    #print("Sex classes: ", le.classes_)
+    #Samething for embarked, but before that fill "NA" for missing values
+    processed_df['Embarked'] = processed_df['Embarked'].fillna("NA")
+    processed_df['Embarked'] = le.fit_transform(processed_df['Embarked'])
+
+    processed_df['Age'] = processed_df['Age'].fillna(0)
+    processed_df['Age'].fillna(processed_df['Age'].mean(), inplace=True)
+    print("Embarked classes: ", le.classes_)
+    return processed_df
+
+'''
+Trains Decision Tree classfier using given dataframe
+@param df dataframe
+@returns trained classifier
+'''
+def train_decision_tree(df):
+    classifier = DecisionTreeClassifier()
+    #FWIW axis=1 means column
+    X = df.drop(['Survived'], axis=1).values
+    Y = df['Survived'].values
+    classifier = classifier.fit(X, Y)
+    return classifier
+
+def main(train_path='data/train.csv', test_path='data/test.csv'):
+    df = read_data_frame(train_path)
     analysis(df)
+    df = preprocess_df(df)
+    print("\nAfter preprocessing...")
+    print(df.count(),"\n")
+    classifier = train_decision_tree(df)
 
+    test_df = read_data_frame(test_path)
+    test_df = preprocess_df(test_df)
+    print("\nAfter preprocessing...")
+    print(test_df.count(),"\n")
+
+    predictions = classifier.predict(test_df)
+    test_df = read_data_frame(test_path)
+    result_df = test_df[['PassengerId']].copy()
+    result_df['Survived'] = pd.Series(predictions, index=result_df.index)
+    print(result_df.columns)
+    result_df.to_csv('prediction1.csv', index=False)
 main()
