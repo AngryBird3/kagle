90e5bf2839a9d735febda554858fc94349bba8f9
AngryBird3 <dhaaraa.darji@gmail.com>
2017-09-17 13:52:03 -0400

    starting to write my first attempt at solving taxi time prediction

---
+++ taxi_time_pred/source/first_attempt.py
@@ -0,0 +1,57 @@
+#!/usr/local/bin/python3
+'''
+Problem: https://www.kaggle.com/c/pkdd-15-taxi-trip-time-prediction-ii
+Resources:
+'''
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.linear_model import LinearRegression # Our ML model
+from sklearn.preprocessing import LabelEncoder # Preprocess to get float
+
+def read_data_frame(filepath):
+    """
+    Read CSV
+    :type filepath: str
+    :rtype: dataframe
+    """
+    df = pd.read_csv(filepath)
+    return df
+    
+def analysis(df):
+    """
+    Print data analysis of a given frame
+    :type df: data frame
+    """
+    print("\n--------------------------------------------------------")
+    print("What all columns/features we have?")
+    print("---------------------------------------------------------")
+    print(df.columns)
+    print("\n---------------------------------------------------------")
+    print("Missing feature/value?")
+    print("---------------------------------------------------------")
+    print(df.columns[df.isnull().any()])
+    print(df.isnull().sum(axis=0))
+    print("\n---------------------------------------------------------")
+    print("Count per feature/column\n")
+    print("---------------------------------------------------------")
+    print(df.count())
+    print("---------------------------------------------------------")
+    print("Describe ...")
+    print("---------------------------------------------------------")
+    print(df.head())
+
+def preprocess_df(df):
+    """
+    Preprocess data
+    :type df: data frame
+    """
+    from geopy.distance import vincenty
+        
+
+def main(train_path='../input/train.csv'):
+    df = read_data_frame(train_path)
+    analysis(df)
+    df = preprocess_df(df)
+    print("\nAfter preprocessing...")
+    print(df.count(),"\n")
+
+main()
